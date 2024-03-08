7cbc8f27314bd22ff81c28bed5283bbfb036d467
Jürgen Schadek <juergen.schadek@familie-schadek.de>
2024-03-08 07:48:11 +0000

    Translations

---
+++ resources/lang/de/statarocket.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+    'article_series' => 'Artikelserien',
+    'author' => 'Autor',
+    'back_to_homepage' => 'Gehe zurück zur Startseite',
+    'breadcrumb' => 'Brotkrumen',
+    'categories' => 'Kategorien',
+    'close_menu' => 'Menü schließen',
+    'contact_form_label_name' => 'Name',
+    'contact_form_label_email' => 'E-Mail',
+    'contact_form_label_comment' => 'Nachricht',
+    'contact_form_label_privacy' => 'Ich habe die
+                                        <a class="font-medium underline" href="/datenschutzerklaerung" target="_blank">Datenschutzerklärung</a>
+                                        gelesen und akzeptiere diese.',
+    'contact_form_button_send' => 'Senden',
+    'dark' => 'Dunkel',
+    'duration' => 'Dauer',
+    'entries_count' => 'Es wurde <span class="font-medium">1</span> Eintrag gefunden. | 
+                            Es wurden <span class="font-medium">:count</span>
+    Einträge gefunden.',
+    'error_404' => 'Fehler 404',
+    'error_404_heading' => 'Hoppla!<br />Seite nicht gefunden.',
+    'error_404_text' => 'Es tut uns leid: Dieser Link ist wohl nicht mehr so ganz frisch.
+                            Es sieht so aus, als wurde diese Seite verschoben oder gelöscht - oder
+                            ein Ninja hat sie gestohlen.',
+    'footer' => 'Fußzeile',
+    'homepage' => 'Startseite',
+    'light' => 'Hell',
+    'newest_article' => 'Dies ist der neueste Artikel.',
+    'newest_video' => 'Dies ist das neueste Video.',
+    'next_article' => 'nächster Artikel',
+    'next_page' => 'nächste Seite',
+    'next_video' => 'nächstes Video',
+    'no_entries_found' => 'Sorry, es wurden keine Einträge gefunden!',
+    'oldest_article' => 'Dies ist der älteste Artikel.',
+    'oldest_video' => 'Dies ist das älteste Video.',
+    'open_menu' => 'Menü öffnen',
+    'pagination' => 'Seitenwechsler',
+    'playlists' => 'Wiedergabelisten',
+    'previous_article' => 'vorheriger Artikel',
+    'previous_page' => 'vorherige Seite',
+    'previous_video' => 'vorheriges Video',
+    'primary_navigation' => 'primäre Navigation',
+    'reading_time' => 'Min. Lesezeit',
+    'related_entries' => 'Ähnliche Einträge',
+    'rss_feed_title' => 'RSS-Feed von',
+    'save_contact' => 'Kontakt speichern',
+    'secondary_navigation' => 'sekundäre Navigation',
+    'system' => 'System',
+    'tags' => 'Schlagwörter',
+    'theme' => 'Thema',
+    'website' => 'Webseite',
+];
+
+?>
---
+++ resources/lang/en/statarocket.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+    'article_series' => 'Article Series',
+    'author' => 'Author',
+    'back_to_homepage' => 'Go back to homepage',
+    'breadcrumb' => 'Breadcrumb',
+    'categories' => 'Categories',
+    'close_menu' => 'Close menu',
+    'contact_form_label_name' => 'Name',
+    'contact_form_label_email' => 'Email',
+    'contact_form_label_comment' => 'Message',
+    'contact_form_label_privacy' => 'I have read the
+                                        <a class="font-medium underline" href="/privacy-policy" target="_blank">Privacy Policy</a>
+                                        and agree with it.',
+    'contact_form_button_send' => 'Send',
+    'dark' => 'Dark',
+    'duration' => 'Duration',
+    'error_404' => 'Error 404',
+    'error_404_heading' => 'UH OH! You\'re lost.<br />Page not found!',
+    'error_404_text' => 'The page you are looking for does not exist. How you got there is a
+                            mystery. But you can click the button below to go back to the
+                            homepage.',
+    'footer' => 'Footer',
+    'homepage' => 'Homepage',
+    'light' => 'Light',
+    'newest_article' => 'This is the newest article.',
+    'newest_video' => 'This is the newest video.',
+    'next_article' => 'Next article',
+    'next_page' => 'Next page',
+    'next_video' => 'Next video',
+    'no_entries_found' => 'Sorry, no entries were found!',
+    'oldest_article' => 'This is the oldest article.',
+    'oldest_video' => 'This is the oldest video.',
+    'open_menu' => 'Open menu',
+    'pagination' => 'Pagination',
+    'playlists' => 'Playlists',
+    'previous_article' => 'Previous article',
+    'previous_page' => 'Previous page',
+    'previous_video' => 'Previous video',
+    'primary_navigation' => 'Primary Navigation',
+    'reading_time' => 'min. Reading time',
+    'related_entries' => 'Related entries',
+    'rss_feed_title' => 'RSS-Feed of',
+    'save_contact' => 'Save contact',
+    'secondary_navigation' => 'Secondary Navigation',
+    'system' => 'System',
+    'tags' => 'Tags',
+    'theme' => 'Theme',
+    'website' => 'Website',
+];
+
+?>
