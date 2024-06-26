da5119112013e4745b35be4f73a53c67ae16089f
Jürgen Schadek <56023985+Juergen-Schadek@users.noreply.github.com>
2024-06-26 13:54:56 +0200

    Routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
+
+Route::statamic('feed', 'feed', [
+    'layout' => 'rss',
+    'content_type' => 'atom',
+]);
+
+Route::statamic('authors/{author}', 'authors.show', [
+    'title' => 'Autor',
+]);
